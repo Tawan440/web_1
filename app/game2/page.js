b61abf29f96fb91900f02f8e3bdedcce9d6027d5
Tawan440 <korapol32@example.com>
2024-06-26 19:15:30 +0700

    cxz

--- app/game2/page.js
+++ app/game2/page.js
@@ -37,7 +37,7 @@ const HeroSection = () => {
       <div className="flex justify-center">
         <button
           onClick={handleClaick}
-          className="mt-40 text-5xl cursor-pointer text-white font-bold relative text-sm md:text-2xl lg:text-4xl xl:text-5xl w-48 md:w-72 h-12 md:h-16 lg:h-20 xl:h-24 text-center bg-gradient-to-r from-violet-500 via-sky-500 to-pink-500 rounded-full z-10 hover:animate-gradient-xy hover:bg-gradient-to-r hover:from-violet-500 hover:via-sky-500 hover:to-pink-500 hover:bg-length-100 before:content-[''] before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0 before:bg-gradient-to-r before:from-violet-500 before:via-sky-500 before:to-pink-500 before:bg-length-400 before:-z-10 before:rounded-30 before:hover:blur-40 before:transition-all before:ease-in-out before:duration-1000 before:hover:bg-length-10 active:bg-violet-700 focus:ring-violet-700 hover:brightness-110 hover:-translate-y-1 hover:border-b-6 active:border-b-2 active:brightness-90 active:translate-y-2"
+          className="mt-40 cursor-pointer text-white font-bold relative text-sm md:text-2xl lg:text-4xl xl:text-5xl w-48 md:w-72 h-12 md:h-16 lg:h-20 xl:h-24 text-center bg-gradient-to-r from-violet-500 via-sky-500 to-pink-500 rounded-full z-10 hover:animate-gradient-xy hover:bg-gradient-to-r hover:from-violet-500 hover:via-sky-500 hover:to-pink-500 hover:bg-length-100 before:content-[''] before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0 before:bg-gradient-to-r before:from-violet-500 before:via-sky-500 before:to-pink-500 before:bg-length-400 before:-z-10 before:rounded-30 before:hover:blur-40 before:transition-all before:ease-in-out before:duration-1000 before:hover:bg-length-10 active:bg-violet-700 focus:ring-violet-700 hover:brightness-110 hover:-translate-y-1 hover:border-b-6 active:border-b-2 active:brightness-90 active:translate-y-2"
         >
           {flipping ? "Flipping a coin..." : "Flip a coin"}
         </button>
@@ -47,3 +47,4 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
+fdsfds
