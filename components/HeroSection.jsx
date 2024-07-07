ed9a9cd81c30ea150bd323006ff93a2a0b770894
Tawan440 <korapol32@gmail.com>
2024-07-07 18:01:29 +0700

    Co-authored-by: Sus1234567890 <Sus1234567890@users.noreply.github.com>

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -9,7 +9,13 @@ import Skibidi3 from "@/public/images/hero-image3.png";
 const HeroSection = () => {
   return (
     <div className="w-full max-w-screen-2xl">
+      <section className="bg-white mt-36 justify-center rounded-lg h-64 w-full border-4 border-white">
+        <p className="text-black justify-center items-center text-8xl font-extrabold . flex flex-col mt-16">
+          Welcome to our website game
+        </p>
+      </section>
       <iframe
+        className="mt-10 justify-center items-center ml-80"
         width="560"
         height="315"
         src="https://www.youtube.com/embed/0lY_TpyAnmc?si=FRnmHWmyTdql76SN"
@@ -19,25 +25,29 @@ const HeroSection = () => {
         referrerpolicy="strict-origin-when-cross-origin"
         allowfullscreen
       ></iframe>
-      <section className="bg-blue-300 mt-36 justify-center rounded-lg h-64 w-full border-4 border-white">
-        <p className="text-black justify-center items-center text-9xl font-extrabold flex flex-col mt-16">
-          RANDOM SECTION
-        </p>
-      </section>
+
       <div className="grid gap-x-16 gap-y-8 grid-cols-3 mt-10 justify-center items-center">
         <Link
           href={
             "https://www.roblox.com/games/17527713600/Citizen-Hustle-Police-Bustle-Tycoon"
           }
-          className=""
+          className="max-w-full"
         >
           <Image
             src={Skibidi1}
             alt="hero image"
-            className="rounded-xl"
+            className="rounded-xl border-4 border-black"
             width={500}
             height={500}
           />
+          <h1 className="text-2xl text-center justify-center items-center bg-white text-black rounded-lg border-4 border-black">
+            a cool game on roblox make by
+            <p>-boat</p>
+            <p>-tawan</p>
+            <p>-taewon</p>
+            <p>-jojo</p>
+            <p>game about police areest mafia and cilizen work for money</p>
+          </h1>
         </Link>
         <Link href={"/game1"} className="">
           <Image
@@ -47,118 +57,18 @@ const HeroSection = () => {
             width={500}
             height={500}
           />
+          <h1 className="text-2xl text-center justify-center items-center bg-white text-black rounded-lg border-4 border-black">
+            click da red button
+            <p>-get more point</p>
+            <p>-more cool</p>
+            <p>-dont use auto click</p>
+            <p>-have fun</p>
+            <p>more have fun</p>
+          </h1>
         </Link>
         <Link href={"/game2"} className="">
           <Image
-            src={Skibidi}
-            alt="hero image"
-            className="rounded-xl"
-            width={500}
-            height={500}
-          />
-        </Link>
-        <Link href={"/"} className="">
-          <Image
-            src={Skibidi}
-            alt="hero image"
-            className="rounded-xl"
-            width={500}
-            height={500}
-          />
-        </Link>
-        <Link href={"/"} className="">
-          <Image
-            src={Skibidi}
-            alt="hero image"
-            className="rounded-xl"
-            width={500}
-            height={500}
-          />
-        </Link>
-        <Link href={"/"} className="">
-          <Image
-            src={Skibidi}
-            alt="hero image"
-            className="rounded-xl"
-            width={500}
-            height={500}
-          />
-        </Link>
-        <Link href={"/"} className="">
-          <Image
-            src={Skibidi}
-            alt="hero image"
-            className="rounded-xl"
-            width={500}
-            height={500}
-          />
-        </Link>
-        <Link href={"/"} className="">
-          <Image
-            src={Skibidi}
-            alt="hero image"
-            className="rounded-xl"
-            width={500}
-            height={500}
-          />
-        </Link>
-        <Link href={"/"} className="">
-          <Image
-            src={Skibidi}
-            alt="hero image"
-            className="rounded-xl"
-            width={500}
-            height={500}
-          />
-        </Link>
-        <Link href={"/"} className="">
-          <Image
-            src={Skibidi}
-            alt="hero image"
-            className="rounded-xl"
-            width={500}
-            height={500}
-          />
-        </Link>
-        <Link href={"/"} className="">
-          <Image
-            src={Skibidi}
-            alt="hero image"
-            className="rounded-xl"
-            width={500}
-            height={500}
-          />
-        </Link>
-        <Link href={"/"} className="">
-          <Image
-            src={Skibidi}
-            alt="hero image"
-            className="rounded-xl"
-            width={500}
-            height={500}
-          />
-        </Link>
-        <Link href={"/"} className="">
-          <Image
-            src={Skibidi}
-            alt="hero image"
-            className="rounded-xl"
-            width={500}
-            height={500}
-          />
-        </Link>
-        <Link href={"/"} className="">
-          <Image
-            src={Skibidi}
-            alt="hero image"
-            className="rounded-xl"
-            width={500}
-            height={500}
-          />
-        </Link>
-        <Link href={"https://nzp.gay/"} className="">
-          <Image
-            src={Skibidi2}
+            src={Skibidi3}
             alt="hero image"
             className="rounded-xl"
             width={500}
